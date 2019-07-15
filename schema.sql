dbe0e1070c648e1c8a92d825aab780ecd9fe4d3d
Nelson <nelsonsprague84@gmail.com>
2019-07-15 12:57:44 -0600

    starter files

---
+++ schema.sql
@@ -0,0 +1,7 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS bloggins;
+-- Creates the "blogger" database --
+CREATE DATABASE bloggins;
+
+SELECT * FROM bloggins.Authors;
+SELECT * FROM bloggins.Posts;
