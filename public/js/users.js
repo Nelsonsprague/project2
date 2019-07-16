189430e507836c563b76a4d09526293df353c1ac
Jeff Andony <jeff.andony@gmail.com>
2019-07-16 13:04:35 -0600

    fixed bugs.

--- public/js/users.js
+++ public/js/users.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
   // Getting references to the name input and author container, as well as the table body
-  var nameInput = $("#author-name");
-  var authorList = $("tbody");
-  var authorContainer = $(".author-container");
+  var nameInput = $("#user-name");
+  var userList = $("tbody");
+  var userContainer = $(".user-container");
   // Adding event listeners to the form to create a new object, and the button to delete
   // an Author
-  $(document).on("submit", "#author-form", handleAuthorFormSubmit);
-  $(document).on("click", ".delete-author", handleDeleteButtonPress);
+  $(document).on("submit", "#user-form", handleuserFormSubmit);
+  $(document).on("click", ".delete-user", handleDeleteButtonPress);
 
   // Getting the initial list of Authors
-  getAuthors();
+  getUsers();
 
   // A function to handle what happens when the form is submitted to create a new Author
-  function handleAuthorFormSubmit(event) {
+  function handleuserFormSubmit(event) {
     event.preventDefault();
     // Don't do anything if the name fields hasn't been filled out
     if (!nameInput.val().trim().trim()) {
       return;
     }
     // Calling the upsertAuthor function and passing in the value of the name input
-    upsertAuthor({
+    upsertUser({
       name: nameInput
         .val()
         .trim()
@@ -27,46 +27,46 @@ $(document).ready(function() {
   }
 
   // A function for creating an author. Calls getAuthors upon completion
-  function upsertAuthor(authorData) {
-    $.post("/api/authors", authorData)
-      .then(getAuthors);
+  function upsertUser(userData) {
+    $.post("/api/users", userData)
+      .then(getUsers);
   }
 
   // Function for creating a new list row for authors
-  function createAuthorRow(authorData) {
+  function createUserRow(userData) {
     var newTr = $("<tr>");
-    newTr.data("author", authorData);
-    newTr.append("<td>" + authorData.name + "</td>");
-    if (authorData.Posts) {
-      newTr.append("<td> " + authorData.Posts.length + "</td>");
+    newTr.data("user", userData);
+    newTr.append("<td>" + userData.name + "</td>");
+    if (userData.Posts) {
+      newTr.append("<td> " + userData.Posts.length + "</td>");
     } else {
       newTr.append("<td>0</td>");
     }
-    newTr.append("<td><a href='/blog?author_id=" + authorData.id + "'>Go to Posts</a></td>");
-    newTr.append("<td><a href='/cms?author_id=" + authorData.id + "'>Create a Post</a></td>");
-    newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
+    newTr.append("<td><a href='/home?user_id=" + userData.id + "'>Go to Posts</a></td>");
+    newTr.append("<td><a href='/post?user_id=" + userData.id + "'>Create a Post</a></td>");
+    newTr.append("<td><a style='cursor:pointer;color:red' class='delete-user'>Delete User</a></td>");
     return newTr;
   }
 
   // Function for retrieving authors and getting them ready to be rendered to the page
-  function getAuthors() {
-    $.get("/api/authors", function(data) {
+  function getUsers() {
+    $.get("/api/users", function(data) {
       var rowsToAdd = [];
       for (var i = 0; i < data.length; i++) {
-        rowsToAdd.push(createAuthorRow(data[i]));
+        rowsToAdd.push(createUserRow(data[i]));
       }
-      renderAuthorList(rowsToAdd);
+      renderUserList(rowsToAdd);
       nameInput.val("");
     });
   }
 
   // A function for rendering the list of authors to the page
-  function renderAuthorList(rows) {
-    authorList.children().not(":last").remove();
-    authorContainer.children(".alert").remove();
+  function renderUserList(rows) {
+    userList.children().not(":last").remove();
+    userContainer.children(".alert").remove();
     if (rows.length) {
       console.log(rows);
-      authorList.prepend(rows);
+      userList.prepend(rows);
     }
     else {
       renderEmpty();
@@ -77,18 +77,18 @@ $(document).ready(function() {
   function renderEmpty() {
     var alertDiv = $("<div>");
     alertDiv.addClass("alert alert-danger");
-    alertDiv.text("You must create an Author before you can create a Post.");
-    authorContainer.append(alertDiv);
+    alertDiv.text("You must create an User before you can create a Post.");
+    userContainer.append(alertDiv);
   }
 
   // Function for handling what happens when the delete button is pressed
   function handleDeleteButtonPress() {
-    var listItemData = $(this).parent("td").parent("tr").data("author");
+    var listItemData = $(this).parent("td").parent("tr").data("user");
     var id = listItemData.id;
     $.ajax({
       method: "DELETE",
-      url: "/api/authors/" + id
+      url: "/api/users/" + id
     })
-      .then(getAuthors);
+      .then(getUsers);
   }
 });
